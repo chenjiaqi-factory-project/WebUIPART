250887f5ca7299815c6ffff298e4e9ca6bab39ed
XinyaoTian <leontian1024@gmail.com>
2019-08-25 18:21:12 +0800

    to docker

--- config.py
+++ config.py
@@ -6,8 +6,10 @@ class Config(object):
 
     SECRET_KEY = os.environ.get('WEB_SERVER_SECRET_KEY') or 'abcdef020301abc8c86f'
 
-    DB_OPS_IP = "127.0.0.1"
-    DB_OPS_PORT = "4999"
+    # DB_OPS_IP = '127.0.0.1'
+    # DB_OPS_PORT = '4999
+    DB_OPS_IP = os.environ.get('DB_OPS_IP')
+    DB_OPS_PORT = os.environ.get('DB_OPS_PORT')
     DB_OPS_URL = DB_OPS_IP + ':' + DB_OPS_PORT
 
     DATA_CSV_PATH = './app/data_process/data.csv'
